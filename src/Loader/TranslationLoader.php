24373e03c8e639d1c8a2a6548a903f9f599d59d6
Tobias Nyholm <tobias.nyholm@gmail.com>
2018-02-12 14:22:04 +0100

    Fixes

--- src/Loader/TranslationLoader.php
+++ src/Loader/TranslationLoader.php
@@ -15,8 +15,9 @@ use Symfony\Component\Finder\Finder;
 use Symfony\Component\Translation\Loader\LoaderInterface;
 use Symfony\Component\Translation\MessageCatalogue;
 use Translation\SymfonyStorage\TranslationLoader as TranslationLoaderInterface;
+use Symfony\Component\Translation\Reader\TranslationReaderInterface;
 
-class TranslationLoader implements TranslationLoaderInterface
+class TranslationLoader implements TranslationLoaderInterface, TranslationReaderInterface
 {
     /**
      * @var LoaderInterface
@@ -43,8 +44,21 @@ class TranslationLoader implements TranslationLoaderInterface
      *
      * @param string           $directory the directory to look into
      * @param MessageCatalogue $catalogue the catalogue
+     *
+     * @deprecated Use read instead.
      */
     public function loadMessages($directory, MessageCatalogue $catalogue)
+    {
+        return $this->read($directory, $catalogue);
+    }
+
+    /**
+     * Loads translation messages from a directory to the catalogue.
+     *
+     * @param string           $directory the directory to look into
+     * @param MessageCatalogue $catalogue the catalogue
+     */
+    public function read($directory, MessageCatalogue $catalogue)
     {
         if (!is_dir($directory)) {
             return;
@@ -59,4 +73,6 @@ class TranslationLoader implements TranslationLoaderInterface
             $catalogue->addCatalogue($this->loader->load($file->getPathname(), $catalogue->getLocale(), $domain));
         }
     }
+
+
 }
